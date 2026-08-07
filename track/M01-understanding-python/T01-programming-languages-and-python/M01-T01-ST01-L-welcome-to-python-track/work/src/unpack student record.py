3276609c93525eda95e74f6d4e8df6589aeb1a0f
Karthik <karthikeyak152@gmail.com>
2026-08-07 16:54:54 +0530

    record

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/unpack student record.py
@@ -0,0 +1,11 @@
+name = input()
+course = input()
+score = int(input()) 
+ 
+student = name , course , score
+
+student_name , student_course , student_score = student
+
+print("Name",name)
+print("Course",course)
+print("Score",score)
