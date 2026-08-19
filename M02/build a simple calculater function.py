622ef084f0ff10b85bc356c4211828a9f421383a
Karthik <karthikeyak152@gmail.com>
2026-08-19 12:37:55 +0530

    done

---
+++ M02/build a simple calculater function.py
@@ -0,0 +1,8 @@
+def calculate(first_number,second_number,operater):
+    return first_number+second_number
+    pass
+first_number = int(input())
+second_number = int(input())
+operater = input()
+result = calculate(first_number,second_number,operater)
+print(result)
